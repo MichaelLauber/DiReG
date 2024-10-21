4d1234d5bcda4e28c4f86807fa950d98c2afb93e
Michael Lauber <michaell2@alfred.exbio.wzw.tum.de>
2024-10-21 17:23:40 +0200

    adjusted landingin page

--- ui/ui_home.R
+++ ui/ui_home.R
@@ -1,18 +1,13 @@
 tabPanel("Home",
          div(id="homeBox",
              
-             h1("Welcome to DiReG"),
-             h3("DiReG was developed for wetlab and computational biologist working on directed reprogramming approaches", align = "center", style = "font-weight: bold;" ),
+             style = "display: flex; flex-direction: column; justify-content: center; align-items: center; padding: 10px; box-sizing: border-box; height: calc(100vh - 150px); overflow: auto; text-align: center;",
              
-             #h3("Any (inferred) set of TF can be investigated in various ways"),
-             img(src='DALLE.png', align = "center", class="img" ),
-             # h4("Explore: Literature curated and predicted directed differentation protocols for various cell types are provided", align = "center" ),
-             # h4("Signature Mining: Sets of predicted TFs can be investigated in various ways and 
-             #    evalutated based on different metrics and compare them to other tools", align="center"),
-             # h4("Discovery: Make a de novo prediction based on epigenome data", align="center")
-             # p(HTML("<b>Explore:</b> Mine the scientific literature and ask specific questions to an state of the art LLM with RAG"), align = "center"),
-             # p(HTML("<b>Signature Mining:</b> Use predictive models and explorative tools to identify sets of transcription factors and compare key metrics"), align = "center"),
-             # p(HTML("<b>Discovery:</b> Leverage epigenome data for de novo predictions"), align = "center")
+             h1("Welcome to DiReG"),
+             h4("DiReG was developed for wetlab and computational biologist working on directed reprogramming approaches", align = "center", style = "font-weight: bold;; font-size: 2vw; margin: 0.5vw;" ),
+             
+             img(src='DALLE.png', align = "center", class="img", style="max-width: 23%; height: auto; margin: auto; display: block;"),
+             
              p(HTML("<b>Explore:</b> Mine the scientific literature and ask specific questions to a state-of-the-art LLM with RAG"), 
                align = "center", 
                style = "font-size: 18px;"),
