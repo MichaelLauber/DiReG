483ba482b54eca11aedd2513407f3e668e0cf3c9
Michael Lauber <michaell2@norm.exbio.wzw.tum.de>
2024-11-02 13:03:56 +0100

    server_tfa cleaned

--- server.R
+++ server.R
@@ -6,7 +6,7 @@
 #
 #    http://shiny.rstudio.com/
 #
- 
+  
   
 # Define server logic required to draw a histogram
 function(input, output, session) {
--- server/server_mining_tfa.R
+++ server/server_mining_tfa.R
@@ -6,6 +6,16 @@ tissues <- colnames(gtexTPM)[-c(1:2)]
 
 vals <- reactiveValues(data = NULL)
 
+
+# Create reactive values to store previous state
+prevState <- reactiveValues(
+  celltype1 = NULL,
+  celltype2 = NULL,
+  data = NULL,
+  res = NULL  # Store previously computed results
+)
+
+
 dataModal <- function(failed = FALSE) {
   modalDialog(
     selectizeInput("selectTissueTFAStart", h3("Start Tissue"),
@@ -26,11 +36,8 @@ dataModal <- function(failed = FALSE) {
       modalButton("Cancel"),
       actionButton("ok", "OK")
     )
-  )
-  
-  
-  
-}
+  )}
+
 
 
 observeEvent(input$selectTissueTFAStart, {
@@ -65,6 +72,16 @@ observeEvent(input$btnTFA, {
 
 observeEvent(input$ok, {
   
+  removeModal() 
+  
+  if (identical(input$selectTissueTFAStart, prevState$celltype1) &&
+      identical(input$selectTissueTFATarget, prevState$celltype2) &&
+      identical(data(), prevState$data)) {
+    
+    # If everything is the same, reuse the cached result
+    res <- prevState$res
+  } else {
+  
   removeModal()
   
   waiter <- waiter::Waiter$new()
@@ -77,16 +94,22 @@ observeEvent(input$ok, {
   celltype1 <- input$selectTissueTFAStart #change to input$
   celltype2 <- input$selectTissueTFATarget #change to input$
   
+  # Cache the current inputs
+  prevState$celltype1 <- celltype1
+  prevState$celltype2 <- celltype2
+  prevState$data <- data()
+  
+  
   #for debugging
   # celltype1 <- "Bladder"
   # celltype2 <- "Liver" 
   
-  dups <- duplicated(gtexTPM$genes)
-  counts <- gtexTPM[!dups,] %>%
-    select(celltype1, celltype2) %>%
+  counts <- gtexTPM %>%
+    filter(!duplicated(genes)) %>%
+    select(all_of(c(celltype1, celltype2))) %>%
     as.matrix()
+  rownames(counts) <- gtexTPM %>% filter(!duplicated(genes)) %>% pull("genes")
   
-  rownames(counts) <- gtexTPM[!dups,] %>% pull("genes")
   
   res <- decoupleR::decouple(mat=counts, #remove after debugging!
                              net=data(),
@@ -102,69 +125,56 @@ observeEvent(input$ok, {
     filter(statistic == "consensus") %>%
     select(-run_id)
   
+  prevState$res <- res
   
   output$tbl_tfa <- renderDT({
-    
     res %>%
+      select(-statistic) %>% 
       DT::datatable() %>%
       formatSignif(columns = c('p_value'), digits = 3)
     
   })
   
-  
+  #enhancement: add column that indates if its input TF and another one that indicates if its downtream of input TF
   output$tfa_plot <-  renderPlotly({
     
-    sample_acts <- res %>% filter(statistic == "consensus")
-    splitted_by_tissue <- split(sample_acts, sample_acts$condition)
-    combined <- inner_join(splitted_by_tissue[[1]], splitted_by_tissue[[2]], by=c("source", "statistic"))
-    combined$greaterPval <- 1/(ifelse(combined$p_value.x < combined$p_value.y, combined$p_value.y, combined$p_value.x))
-    combined$symbol <- ifelse(combined$source %in% inputTFs(),  'diamond', 'circle')
-    combined$lineColor <- ifelse(combined$source %in% c("inputTFs()"),  'red', 'white')
+    combined <- inner_join(
+      res %>% filter(statistic == "consensus") %>% split(.$condition) %>% `[[`(1),
+      res %>% filter(statistic == "consensus") %>% split(.$condition) %>% `[[`(2),
+      by = c("source", "statistic")
+    )
     
-    max <- max(c(combined$score.x, combined$score.y))
-    min <- min(c(combined$score.x, combined$score.y))
+    combined$greaterPval <- 1 / pmax(combined$p_value.x, combined$p_value.y)
+    combined$symbol <- ifelse(combined$source %in% inputTFs(), "diamond", "circle")
+    combined$lineColor <- ifelse(combined$source %in% inputTFs(), "black", "white")
     
-    xtitle <- unique(combined$condition.x)
-    ytitle <- unique(combined$condition.y)
+    max_val <- max(c(combined$score.x, combined$score.y))
+    min_val <- min(c(combined$score.x, combined$score.y))
+    
     plot_ly(combined,
-            x = ~score.x,
-            y = ~score.y,
-            marker = list(size = 10,
-                          symbol = ~symbol,
-                          color = ~greaterPval,
-                          #color = 'rgba(255, 182, 193, .9)',
-                          line = list(color = ~lineColor, width = 2)
-            ),
-            text = ~paste(combined$source,
-                          "<br>",
-                          "p value in", combined$condition.x,": ", combined$p_value.x,
-                          '<br>',
-                          "p value in", combined$condition.y,": ", combined$p_value.y
-            )
-    ) %>%
-      layout(shapes =
-               list(
-                 list(
-                   type = "line",
-                   x0 = min,
-                   x1 = max,
-                   xref = "x",
-                   y0 = min,
-                   y1 = max,
-                   yref = "y",
-                   line = list(color = "black", width = 1, dash = 'dash')
-                 )
-               )
-             ,
-             xaxis = list(title = paste0("TFA in ", xtitle)),
-             yaxis = list(title = paste0("TFA in ", ytitle)),
-             annotations = list(
-               list(x = 0.05, y = 1, xref = 'paper', yref = 'paper', text = 'Diamond: Input TF', showarrow = FALSE, align = 'left'),
-               list(x = 0.05, y = 0.95, xref = 'paper', yref = 'paper', text = 'Color: p-value', showarrow = FALSE, align = 'left')
-             )
-      )
+            x = ~score.x, y = ~score.y,
+            type = 'scatter',  
+            mode = 'markers', 
+            marker = list(size = 10, symbol = ~symbol, color = ~greaterPval,
+                          line = list(color = ~lineColor, width = 2)),
+            text = ~paste(source, "<br>", "p value in", condition.x, ": ", p_value.x,
+                          "<br>", "p value in", condition.y, ": ", p_value.y)) %>%
+      layout(shapes = list(
+        list(type = "line", x0 = min_val, x1 = max_val, xref = "x",
+             y0 = min_val, y1 = max_val, yref = "y",
+             line = list(color = "black", width = 1, dash = "dash"))
+      ),
+      xaxis = list(title = paste0("TFA in ", unique(combined$condition.x))),
+      yaxis = list(title = paste0("TFA in ", unique(combined$condition.y))),
+      annotations = list(
+        list(x = 0.05, y = 1, xref = "paper", yref = "paper",
+             text = "Diamond: Input TF", showarrow = FALSE, align = "left"),
+        list(x = 0.05, y = 0.95, xref = "paper", yref = "paper",
+             text = "Color: p-value", showarrow = FALSE, align = "left")
+      ))
     
+
     
   })
-  
+  }
 })
