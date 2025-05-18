4c74d8aef178329fb7e7e1d97dfeb2a59c03067e
Michael Lauber <michaell2@norm.exbio.wzw.tum.de>
2025-05-18 15:45:03 +0200

    Minor Changes Landing Page

--- ui/ui_home.R
+++ ui/ui_home.R
@@ -3,12 +3,12 @@ tabPanel("Home",
              
              style = "display: flex; flex-direction: column; justify-content: center; align-items: center; padding: 10px; box-sizing: border-box; height: calc(100vh - 150px); overflow: auto; text-align: center;",
              
-             h1("Welcome to DiReG"),
+             h1("Welcome to DiReG", style = "font-weight: bold;; font-size: 2vw; margin: 0.5vw;"),
              h4("An App developed for Scientists working on Direct Reprogramming", align = "center", style = "font-weight: bold;; font-size: 2vw; margin: 0.5vw;" ),
              
-             img(src='DALLE.png', align = "center", class="img", style="max-width: 23%; height: auto; margin: auto; display: block;"),
+             img(src='avocado2pumpkin.png', align = "center", class="img", style="max-width: 23%; height: auto; margin: auto; display: block;"),
              
-             p(HTML("<b>Explore:</b> Mine the scientific literature and ask specific questions to a state-of-the-art LLM with RAG"), 
+             p(HTML("<b>Explore:</b> Explore the reprogramming literature via a Paperqa a state-of-the-art LLM with RAG and browse already inferred protocols from various tools"), 
                align = "center", 
                style = "font-size: 18px;"),
              
@@ -16,7 +16,10 @@ tabPanel("Home",
                align = "center", 
                style = "font-size: 18px;"),
              
-             p(HTML("<b>Discovery:</b> Leverage epigenome data for de novo predictions"), 
+             p(HTML("<b>Discovery:</b> Leverage epigenome data for de novo predictions via AME"), 
+               align = "center", 
+               style = "font-size: 18px;"),
+             p(HTML("For detailed explanation of all the apps' functionality please go to the <b>Documentation</b> section"), 
                align = "center", 
                style = "font-size: 18px;")
          )
