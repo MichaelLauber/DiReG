4e67aa3ce31ab27bb20a2c0f0c55e8d04232b1f4
Michael Lauber <michaell2@norm.exbio.wzw.tum.de>
2025-07-19 21:00:34 +0200

    Rmved display of TRUE in Documentation

--- app/server/server_doc.R
+++ app/server/server_doc.R
@@ -8,23 +8,36 @@ observeEvent(input$actnBtnSignature, {
   
   output$tabsetOutput <- renderUI({
     tabsetPanel(
-      tabPanel("OR Analysis", source("doc/doc_ORA.R", local=T)),
-      tabPanel("GSEA", source("doc/doc_GSEA.R", local=T)),
-      tabPanel("Tissue Expression", source("doc/doc_expression.R", local=T)),
-      tabPanel("TF Cofactors", source("doc/doc_tcof.R", local=T)),
-      tabPanel("TT-TF Interaction", source("doc/doc_tftf.R", local=T)),
-      tabPanel("Isoform Potential", source("doc/doc_isoform.R", local=T)),
-      tabPanel("TFA Analysis", source("doc/doc_TFA.R", local=T))
+      tabPanel("OR Analysis", {
+        source("doc/doc_ORA.R", local=TRUE)$value
+      }),
+      tabPanel("GSEA", {
+        source("doc/doc_GSEA.R", local=TRUE)$value
+      }),
+      tabPanel("Tissue Expression", {
+        source("doc/doc_expression.R", local=TRUE)$value
+      }),
+      tabPanel("TF Cofactors", {
+        source("doc/doc_tcof.R", local=TRUE)$value
+      }),
+      tabPanel("TT-TF Interaction", {
+        source("doc/doc_tftf.R", local=TRUE)$value
+      }),
+      tabPanel("Isoform Potential", {
+        source("doc/doc_isoform.R", local=TRUE)$value
+      }),
+      tabPanel("TFA Analysis", {
+        source("doc/doc_TFA.R", local=TRUE)$value
+      })
     )
   })
-  
 })
 
 # When actnBtnExplore is clicked, update uiOutput with different text
 observeEvent(input$actnBtnExplore, {
   activeButton("actnBtnExplore")
   output$sectionContent <- renderUI({
-    source("doc/doc_explore.R", local=T)
+    source("doc/doc_explore.R", local=TRUE)$value
   })
   output$tabsetOutput <- NULL
 })
@@ -33,8 +46,7 @@ observeEvent(input$actnBtnExplore, {
 observeEvent(input$actnBtnDiscovery, {
   activeButton("actnBtnDiscovery")
   output$sectionContent <- renderUI({
-    source("doc/doc_discovery.R", local=T)
-    # You can replace this with the content you want to display
+    source("doc/doc_discovery.R", local=TRUE)$value
   })
   output$tabsetOutput <- NULL
 })
@@ -43,8 +55,3 @@ output$showWelcome <- reactive({
   is.null(activeButton())
 })
 outputOptions(output, "showWelcome", suspendWhenHidden = FALSE)
-
-# # Render the table within the specified div
-# output$mockTable <- renderDT({
-#   datatable(mock_data, options = list(dom = 't'))
-# })
