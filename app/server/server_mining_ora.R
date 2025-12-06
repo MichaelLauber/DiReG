8dd92c3e51e264346d841ce8f448dc3fc9b5a1ec
Michael Lauber <michaell2@norm.exbio.wzw.tum.de>
2025-12-06 12:17:48 +0100

    adjusted ora prompt

--- app/server/server_mining_ora.R
+++ app/server/server_mining_ora.R
@@ -193,14 +193,15 @@
       showModal(modalDialog("Please Upload an API Key [For test purposes an API Key is provided by us]", easyClose = TRUE))
     }
     
+    
     user_prompt <- paste0("I am scientist interested in direct reprogramming. 
-                        I have a gene set that gets activated when overexpressing a set of TFs and have performed GSEA on it.
+                        I have a gene set that gets activated when overexpressing a set of TFs and have performed over-representation analysis on it.
                         Tell me do the enriched genesets show specifity for: ", 
                           input$user_prompt_ora, 
                           ". These are the enriched genesets: ", 
                           paste(cache$enrichData$result$term_name, 
                                 collapse = ", "),
-                          ". Return Answer with Markdown Formatting. It will be displayed within an web-application."
+                          ". Return Answer with Markdown Formatting. It will be displayed within an web-application. Keep it concise. Do not ask follow up questions."
                           )
     
     shinyjs::runjs("$('#llm_response_ora').text('Generating response, please wait...');")
@@ -244,8 +245,8 @@
       })
     } else {
       # Print an error message if the request fails
-      cat("Error: Unable to retrieve a response. Status code:", status_code(response), "\n")
-      response_to_display <- paste0("Error: Unable to retrieve a response. Status code:", status_code(response), "\n")
+      cat("Error: Unable to retrieve a response. Status code:", httr::status_code(response), "\n")
+      response_to_display <- paste0("Error: Unable to retrieve a response. Status code:", httr::status_code(response), "\n")
       
       output$llm_response_ora <- renderUI({
         HTML(markdown::markdownToHTML(text = response_to_display, fragment.only = TRUE))
